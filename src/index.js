ceb2269f98ee32d64bed9588c798b08cb85ca4e9
Matusevich86 <alex.matusevich86@gmail.com>
2023-01-24 19:25:43 +0100

    feat: completed task

--- src/index.js
+++ src/index.js
@@ -38,8 +38,38 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
+    const length = 10;
+    const pattern = new RegExp(".{1," + length + "}", "ig");
+    let arr = expr.match(pattern);
+    let newArr = [];
+    let finStr = [];
+    for (let v of arr) {
+      const s00 = "00";
+      const replace00 = "";
+      const s10 = "10";
+      const replace10 = ".";
+      const s11 = "11";
+      const replace11 = "-";
+      const space = "**********";
+      const replaceSpace = " ";
+      newArr.push(
+        v
+          .replaceAll(s00, replace00)
+          .replaceAll(s10, replace10)
+          .replaceAll(s11, replace11)
+          .replaceAll(space, replaceSpace)
+      );
+    }
+    //console.log(MORSE_TABLE)
+    //console.log(newArr)
+    //console.log(MORSE_TABLE[newArr[0]])
+    newArr.map((item, index) => {
+      if (item in MORSE_TABLE) {
+        finStr.push(MORSE_TABLE[item]);
+      } else {finStr.push(' ');}
+    });
+    return finStr.join('')
+  }
 
 module.exports = {
     decode
